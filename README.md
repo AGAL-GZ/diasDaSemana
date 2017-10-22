b2b26c77f5092cb8348d2160b3df014c71b5fed8
Antom M.G <meigaran@gmail.com>
2017-10-22 19:32:22 +0200

    Primeira subida

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # diasDaSemana
-Campanha em colaboraçom com A Mesa na qual fomentamos a utilizaçom dos dias da semana por feiras.  V
+Campanha em colaboraçom com A Mesa na qual fomentamos a utilizaçom dos dias da semana por feiras.
+
+# Wordpress
+Editar os ficheiros header.php e style.css do tema.
+Para elo dirigir-se a "Apresentação:Editor".
+
+Engadir os conteudos dos ficheiros js/diasDaSemana.js e css/diasDaSemana.css nos respeitivos ficheiros
+
+# Estilo
+
+## Fonte
+- Fonte: HelveNeuBlaCon
+- Alternativo: Arial Black
+- Tamanho: 14
+
+# Cores:
+
+## Violeta
+- Violeta suave: #e23e83FF
+- Violeta escuro: #8b0067FF
---
+++ js/diasDaSemana.js
@@ -0,0 +1,30 @@
+<script type="text/javascript"><!--
+
+/* Devolve a imagem a mostrar */
+function getDiaDaSemanaImg(){
+  /* Textos a mostrar */
+  var mes = new Array('janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'agosto', 'outubro', 'novembro', 'dezembro');
+  var feira = new Array('Domingo', 'Segunda feira', 'Terça feira', 'Quarta feira', 'Quinta feira', 'Sexta feira', 'Sábado');
+
+  var path = "/wp-content/uploads/2017/10/_" /* path da imagem */
+  var polvo = "polvo_violeta" /* raiz do nome da imagem */
+
+  var currentdate = new Date(); /* obtemos a informaçom do dia de hoje */
+  var diaDende0 = currentdate.getDay(); /* currentdate.getDay devolve 0: domingo, 1: segunda, ... 6 sábado */
+
+  switch (diaDende0){
+    case 0: this.img=path+polvo+'_d.png';break;
+    case 1: this.img=path+polvo+'_2.png';break;
+    case 2: this.img=path+polvo+'_3.png';break;
+    case 3: this.img=path+polvo+'_4.png';break;
+    case 4: this.img=path+polvo+'_5.png';break;
+    case 5: this.img=path+polvo+'_6.png';break;
+    case 6: this.img=path+polvo+'_s.png';break;
+  }
+
+  this.feira = feira[diaDende0]; /* Domingo, Segunda feira, ... */
+  this.mes = currentdate.getDate() + 'de' + mes[currentdate.getMoth()]; /* 22 de fevereiro */
+  this.img = path+polvo+ext;
+  return this;
+}
+//--></script>
